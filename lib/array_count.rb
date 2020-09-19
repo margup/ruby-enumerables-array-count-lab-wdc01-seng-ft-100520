705e314129cc3413bcd00f0f7beb42c861e94c23
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-19 17:09:50 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,14 +1,10 @@
 def count_strings(array)
-  num = 0 
-  while num < array.length do
-   
-    if array[num] == Integer
-      return true
-  end
-     
+  array.count do |num| 
+    if num == Integer
       num += 1 
-    end
   end
+end
+end
  
   # Return the total number of strings in the provided array using the count enumerable
 
