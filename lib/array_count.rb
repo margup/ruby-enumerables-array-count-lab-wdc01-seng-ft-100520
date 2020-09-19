928894d4bbf1447f659a79882025a6fb468dfb78
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-19 16:53:08 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,7 +5,6 @@ def count_strings(array)
     end
   end
   # Return the total number of strings in the provided array using the count enumerable
-end
 
 def count_empty_strings(array)
   array.count do 
